4581b5ac0fbbcffbc7be7eafde249190c70f62d2
amalla4 <amalla4@students.towson.edu>
2024-03-16 01:34:18 -0400

    connected to postgres db container and added data successfully

---
+++ databasepostgre/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO widgets (id, name, purpose) VALUES
+(1, 'Widget A', 'Used for testing purposes.'),
+(2, 'Widget B', 'Designed for ent.'),
+(3, 'Widget C', 'Productivity widget.'),
+(4, 'Widget A', 'Activity monitor widget.'),
+(5, 'Widget E', 'Overall testing widget.');
