179b0e6a60971758e1a53a9efd7c93866d9d1d37
amalla4 <amalla4@students.towson.edu>
2024-03-22 00:01:13 -0400

    fixed sql statement in BookDaoImpl, integration test now passing

--- jdbcTemplateTest/src/main/java/com/ap/jdbcTemplateTest/dao/impl/BookDaoImpl.java
+++ jdbcTemplateTest/src/main/java/com/ap/jdbcTemplateTest/dao/impl/BookDaoImpl.java
@@ -22,7 +22,7 @@ public class BookDaoImpl implements BookDao {
     @Override
     public void create(Book book) {
         jdbcTemplate.update(
-                "INSERT INTO books (isbn, title, author)id) VALUES (?, ?, ?)",
+                "INSERT INTO books (isbn, title, author_id) VALUES (?, ?, ?)",
                 book.getIsbn(),
                 book.getTitle(),
                 book.getAuthorId()
